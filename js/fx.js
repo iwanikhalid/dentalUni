483ecc21ec3572a9622184a9326048d07f8f0900
Iwani Khalid <iwanikhalid@gmail.com>
2012-12-14 17:33:17 +0800

    Indexpage

---
+++ js/fx.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+	// Slideshow
+	$(function(){
+		$('#slides').slides({
+			preload: true,
+			preloadImage: 'img/loading.gif',
+			play: 5000,
+			pause: 2500,
+			hoverPause: true
+		});
+	});
+
+});
